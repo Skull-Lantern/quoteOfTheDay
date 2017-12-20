9f91752b9c7dfba9f0b9c967567487b68da44d69
SockHat-Dev <tamim.hamoudi@gmail.com>
2017-12-20 11:21:02 +0200

    Big added to the picture

--- quoteOfTheDay.py
+++ quoteOfTheDay.py
@@ -1,7 +1,7 @@
 import requests
 import gi
 gi.require_version("Notify", "0.7")
-from gi.repository import Notify
+from gi.repository import Notify,GdkPixbuf
 
 getRequestUrl='https://quotes.rest/qod'
 getRequestAction=requests.get(getRequestUrl)
@@ -9,8 +9,16 @@ jsonRequestData=getRequestAction.json()
 
 qoute=jsonRequestData['contents']['quotes'][0]['quote']
 author=jsonRequestData['contents']['quotes'][0]['author']
+
+# Use GdkPixbuf to create the proper image type
+image = GdkPixbuf.Pixbuf.new_from_file("big-G.png")
+
+
 Notify.init ("Quote of the Day")
 quoteOfTheDay=Notify.Notification.new ("Quote of the Day",
-                               qoute+" '"+author+"'",
-                               "dialog-information")
-quoteOfTheDay.show ()
+                               qoute+" '"+author+"'",)
+# Use the GdkPixbuf image
+quoteOfTheDay.set_icon_from_pixbuf(image)
+quoteOfTheDay.set_image_from_pixbuf(image)
+
+quoteOfTheDay.show()
